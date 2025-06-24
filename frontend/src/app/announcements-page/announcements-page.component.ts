364f38451e9721cf2cec89a2100fd9e720969abe
carlossr404 <carlossr404@gmail.com>
2025-06-24 15:26:59 -0500

    Created modal component to be used across different webpages in the app

--- frontend/src/app/announcements-page/announcements-page.component.ts
+++ frontend/src/app/announcements-page/announcements-page.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./announcements-page.component.css']
 })
 export class AnnouncementsPageComponent {
+  showCreateAnnouncementModal = false;
 
+  openCreateAnnouncement() {
+    this.showCreateAnnouncementModal = true;
+  }
+
+  closeCreateAnnouncement() {
+    this.showCreateAnnouncementModal = false;
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { TeamsPageComponent } from './teams-page/teams-page.component';
 import { TeamsCardComponent } from './teams-page/teams-card/teams-card.component';
 import { UsersPageComponent } from './users-page/users-page.component';
-import { CreateAnnouncementComponent } from './announcements-page/create-announcement/create-announcement.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +27,7 @@ import { CreateAnnouncementComponent } from './announcements-page/create-announc
     TeamsPageComponent,
     TeamsCardComponent,
     UsersPageComponent,
-    CreateAnnouncementComponent
+    ModalComponent
   ],
   imports: [
     BrowserModule,
