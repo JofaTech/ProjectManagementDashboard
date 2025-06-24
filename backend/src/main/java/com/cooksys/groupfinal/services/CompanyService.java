16915b28d6f33a5113d38ffe3b0ddc95157a4c86
Chaz Dooley <80858674+c-engineering-dooley@users.noreply.github.com>
2025-06-24 15:36:14 -0400

    added posts and deletes for company service

--- backend/src/main/java/com/cooksys/groupfinal/services/CompanyService.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/CompanyService.java
@@ -6,6 +6,7 @@ import com.cooksys.groupfinal.dtos.AnnouncementDto;
 import com.cooksys.groupfinal.dtos.FullUserDto;
 import com.cooksys.groupfinal.dtos.ProjectDto;
 import com.cooksys.groupfinal.dtos.TeamDto;
+import com.cooksys.groupfinal.dtos.UserRequestDto;
 
 public interface CompanyService {
 
@@ -16,5 +17,13 @@ public interface CompanyService {
 	Set<TeamDto> getAllTeams(Long id);
 
 	Set<ProjectDto> getAllProjects(Long companyId, Long teamId);
+
+    FullUserDto addUser(Long id, UserRequestDto uRequestDto);
+
+	AnnouncementDto addAnnouncement(Long id, AnnouncementDto aDto);
+
+	void removeTeam(Long companyId, Long teamId);
+
+	void deleteAnnouncement(Long companyId, Long announcementId);
 
 }
