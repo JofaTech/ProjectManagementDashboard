aad7c80ca4320ace75af706d9dae2fe4a62e03c7
carlossr404 <carlossr404@gmail.com>
2025-06-24 23:08:52 -0500

    styled modal for users page, got functionality with placeholder code and dummy data. Pending backend integration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +32,8 @@ import { ModalComponent } from './modal/modal.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/users-page/users-page.component.ts
+++ frontend/src/app/users-page/users-page.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
   styleUrls: ['./users-page.component.css']
 })
-export class UsersPageComponent {
+export class UsersPageComponent implements OnInit {
 
   // Dummy data for users:
   users = [
@@ -17,6 +18,49 @@ export class UsersPageComponent {
 
   showUserOverlay = false;
 
+  userForm!: FormGroup;
+  dropdownOpen = false;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.userForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      adminRole: [null, Validators.required]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator = (form: FormGroup) => {
+    const pw = form.get('password')!.value;
+    const cpw = form.get('confirmPassword')!.value;
+    return pw === cpw ? null : { passwordMismatch: true };
+  };
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectRole(value: boolean) {
+    this.userForm.patchValue({ adminRole: value });
+    this.dropdownOpen = false;
+  }
+
+  submitNewUser() {
+    if (this.userForm.valid) {
+      console.log('User data to submit:', this.userForm.value);
+      // TODO: connect to backend here later
+      this.closeAddUser();
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
+
+  // Methods for displaying Modal
   openAddUser() {
     this.showUserOverlay = true;
   }
