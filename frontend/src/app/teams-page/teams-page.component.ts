6d3966d677403c844ebb8ff9e578964920511228
carlossr404 <carlossr404@gmail.com>
2025-06-24 21:19:00 -0500

    finished styling & displaying dummy data for the teams page and teams-modal component. Added placeholders for integrating with backend

--- frontend/src/app/teams-page/teams-page.component.ts
+++ frontend/src/app/teams-page/teams-page.component.ts
@@ -35,13 +35,56 @@ export class TeamsPageComponent {
     }
   ]
 
+  // Form fields
+  teamName = '';
+  description = '';
+
+  // Dummy data for available members
+  availableMembers = ['Chris P.', 'Wil M.', 'Helena M.', 'Kenny W.']
+
+  selectedMembers: string[] = [];
+
   showNewTeamModal = false;
 
+  // Methods for selecting + removing members
+  onMemberSelected(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    const member = selectElement.value;
+
+    // Make sure member exists and is not a part of selected members array already
+    if (member && !this.selectedMembers.includes(member)) {
+      this.selectedMembers.push(member);
+    }
+  }
+
+  removeMember(member: string) {
+    this.selectedMembers = this.selectedMembers.filter(m => m !== member);
+  }
+
+  // Open and close modal methods
   openNewTeamModal() {
     this.showNewTeamModal = true;
   }
 
   closeNewTeamModal() {
     this.showNewTeamModal = false;
+  }
+
+  // Method for submitting new team to database
+  submitNewTeam() {
+    // TODO: Implement POST logic to submit new team
+    console.log('submitNewTeam() called. Implementation pending. New Team:', {
+      teamName: this.teamName,
+      description: this.description,
+      members: this.selectedMembers
+    })
+
+    // Close team modal
+    this.closeNewTeamModal()
+
+    // Clear form data for next time
+    this.teamName = '';
+    this.description = '';
+    this.selectedMembers = [];
   }
 }
