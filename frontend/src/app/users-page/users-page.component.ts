15d82503d019eb5286d676df8afa38d7b8b87cb1
carlossr404 <carlossr404@gmail.com>
2025-06-24 21:54:13 -0500

    User registry created, styled, and working with dummy data. Pending backend integration

--- frontend/src/app/users-page/users-page.component.ts
+++ frontend/src/app/users-page/users-page.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class UsersPageComponent {
 
+  // Dummy data for users:
+  users = [
+    { name: 'Chris Purnell', email: 'yocizzle@gmail.com', active: true, admin: true, status: 'JOINED' },
+    { name: 'Kenny Worth', email: 'kmoney@gmail.com', active: true, admin: true, status: 'JOINED' },
+    { name: 'Will Marttala', email: 'wamizzle@gmail.com', active: false, admin: false, status: 'PENDING' },
+    { name: 'Helena Makendengue', email: 'hmasizzle@gmail.com', active: false, admin: false, status: 'PENDING' },
+  ];
+
+  showUserOverlay = false;
+
+  openAddUser() {
+    this.showUserOverlay = true;
+  }
+
+  closeAddUser() {
+    this.showUserOverlay = false;
+  }
+
 }
