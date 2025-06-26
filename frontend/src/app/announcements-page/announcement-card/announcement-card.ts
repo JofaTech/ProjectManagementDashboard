5b064f197fffcede685e4a40fa7424aef169ed69
carlossr404 <carlossr404@gmail.com>
2025-06-25 22:34:58 -0500

    announcements page displaying announcements for selected company

--- frontend/src/app/announcements-page/announcement-card/announcement-card.ts
+++ frontend/src/app/announcements-page/announcement-card/announcement-card.ts
@@ -1,4 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+// Profile interface
+interface Profile {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
+
+// Basicuser interface
+interface BasicUser {
+  id: number;
+  profile: Profile;
+  isAdmin: boolean;
+  active: boolean;
+  status: string;
+}
+
+// Announcement interface
+interface Announcement {
+  id: number;
+  date: number; // timestamp
+  title: string;
+  message: string;
+  author: BasicUser;
+}
 
 @Component({
   selector: 'app-announcement-card',
@@ -6,5 +32,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./announcement-card.css']
 })
 export class AnnouncementCard {
+  @Input() announcement!: Announcement;
 
+  get formattedDate(): string {
+    // Format date
+    const dateObj = new Date(this.announcement.date);
+    return dateObj.toLocaleDateString('en-US', {
+      year: 'numeric', month: 'long', day: 'numeric'
+    });
+  }
+
+  get authorName(): string {
+    const author = this.announcement.author;
+    const profile = author?.profile;
+    if (!profile) {
+      return 'Unknown';
+    }
+    return `${profile.firstName} ${profile.lastName}, ${author.status}`;
+  }
 }
