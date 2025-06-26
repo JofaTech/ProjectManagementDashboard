4366920aef0c1d79985678a1aa338d9984014e6d
naumgh <naum.hoffman@gmail.com>
2025-06-26 10:05:07 -0700

    commiting user-session services

---
+++ frontend/src/app/services/user-session.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { BasicUserDto } from './basic-user.dto';
+import { UrlSerializer } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserSessionService {
+
+  private user: BasicUserDto | null = null;
+
+  constructor() { }
+
+  setUser(user:BasicUserDto):void{
+    this.user=user;
+  }
+  
+  getUser():BasicUserDto | null{
+    return this.user;
+  }
+
+  isLoggedIn():boolean {
+    return this.user != null;
+  }
+
+  isAdmin():boolean{
+    if (this.user && this.user.admin) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logout():void{
+    this.user=null;
+  }
+  
+}
